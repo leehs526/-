4bf1eee15fcf6d72feebdde6c7caa5b3d34ad1b5
이한설 <juicyiceyg@gmail.com>
2026-07-08 00:26:57 +0900

    중앙값.py

--- 코테 0/중앙값.py
+++ 코테 0/중앙값.py
@@ -1,2 +1,8 @@
 def solution(array):
     return sorted(array)[len(array) // 2]
+
+def solution(array):
+    answer = sorted(array)
+    n = len(answer)
+    median = answer[n//2]
+    return median
