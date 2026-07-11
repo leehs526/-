8c611a65d039d22c7add96bb8a5fdf25281f626e
이한설 <juicyiceyg@gmail.com>
2026-07-11 22:35:43 +0900

    Update 문자열 삭제.py

--- 코테 0/문자열 삭제.py
+++ 코테 0/문자열 삭제.py
@@ -1,2 +1,11 @@
+1)
 def solution(my_string, letter):
     return my_string.replace(letter,"")
+    
+2)
+def solution(my_string, letter):
+    answer = ''
+    for string in my_string:
+        if string != letter:
+            answer += string
+    return answer
