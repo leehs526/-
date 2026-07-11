760f714c9bb04509503e7cc361d16f3e34e6a9d6
이한설 <juicyiceyg@gmail.com>
2026-07-11 22:29:07 +0900

    Update 문자열 반복.py

--- 코테 0/문자열 반복.py
+++ 코테 0/문자열 반복.py
@@ -1,5 +1,14 @@
+1)
 def solution(my_string, n):
     answer = ""
     for c in my_string:
         answer += c * n
     return answer
+
+2)
+def solution(my_string, n):
+    return ''.join(i*n for i in my_string)
+
+3)
+def solution(my_string, n):
+    return "".join(map(lambda x: x*n, my_string))
