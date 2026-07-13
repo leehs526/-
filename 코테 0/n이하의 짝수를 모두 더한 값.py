c654a878127e0257b9c88c54b64b5753bafb74a3
이한설 <juicyiceyg@gmail.com>
2026-07-13 22:09:46 +0900

    n이하의 짝수를 모두 더한 값.py 업데이트

--- 코테 0/n이하의 짝수를 모두 더한 값.py
+++ 코테 0/n이하의 짝수를 모두 더한 값.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = 0
-    for i in range(1,n+1):
+    for i in range(1,n+1): #range 대신 n을 쓰는건 안됨, n이 정수이기 때문
         if i % 2 == 0:
             answer += i
     return answer
