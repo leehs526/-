c1296816d7d23b6ac0a867f513b5b89fee157e2a
이한설 <juicyiceyg@gmail.com>
2026-07-16 23:22:25 +0900

    Update 삼각형의 완성조건.py

--- 코테 0 O/삼각형의 완성조건.py
+++ 코테 0 O/삼각형의 완성조건.py
@@ -1,5 +1,10 @@
+1)
 def solution(sides):
     if max(sides) < sum(sides) - max(sides):
         return 1
     else:
         return 2
+
+2)
+def solution(sides):
+    return 1 if max(sides) < sum(sides) - max(sides) else 2
