66e9ee70e9f56fa1ccbd657aa7b5ff86f6172621
이한설 <juicyiceyg@gmail.com>
2026-07-16 23:00:57 +0900

    Update 숨은 자연수의 합.py

--- 코테 0/숨은 자연수의 합.py
+++ 코테 0/숨은 자연수의 합.py
@@ -1,6 +1,11 @@
+1)
 def solution(my_string):
     answer = 0
     for i in my_string:
         if i.isdigit(): #isdigit 숫자인지 확인하는 함수, ()은 함수호출
             answer += int(i)
     return answer
+
+2)
+def solution(my_string):
+    return sum(int(i) for i in my_string if i.isdigit())
